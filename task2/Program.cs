826d81dae7268f4baec384afa4842c3b01002b80
Anton <17.11.1993@bk.ru>
2022-08-29 21:15:42 +0300

    task2=ok

--- task2/Program.cs
+++ task2/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая будет выдавать
+// название дня недели по заданному номеру.
+// 3 -> Среда
+// 5 - > Пятница
+Console.Write("Введите целое число ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+if(a == 1)Console.WriteLine("Понедельник");
+else if(a == 2)Console.WriteLine("Вторник");
+else if(a == 3)Console.WriteLine("Среда");
+else if(a == 4)Console.WriteLine("Четверг");
+else if(a == 5)Console.WriteLine("Пятница");
+else if(a == 6)Console.WriteLine("Суббота");
+else if(a == 7)Console.WriteLine("Воскресение");
+else Console.Write("Число не в диапозоне 1-7");
+
+
