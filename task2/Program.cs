b10ef54a3147383e5a6689ad723afc11b1c0370f
Anton <17.11.1993@bk.ru>
2022-08-30 21:39:09 +0300

    add_task_3

--- task2/Program.cs
+++ task2/Program.cs
@@ -2,16 +2,14 @@
 // название дня недели по заданному номеру.
 // 3 -> Среда
 // 5 - > Пятница
+
 Console.Write("Введите целое число ");
 int a = Convert.ToInt32(Console.ReadLine());
-
-if(a == 1)Console.WriteLine("Понедельник");
-else if(a == 2)Console.WriteLine("Вторник");
-else if(a == 3)Console.WriteLine("Среда");
-else if(a == 4)Console.WriteLine("Четверг");
-else if(a == 5)Console.WriteLine("Пятница");
-else if(a == 6)Console.WriteLine("Суббота");
-else if(a == 7)Console.WriteLine("Воскресение");
+if(a == 1) Console.Write("Понедельник");
+else if(a == 2) Console.Write("Вторник");
+else if(a == 3) Console.Write("Среда");
+else if(a == 4) Console.Write("Четверг");
+else if(a == 5) Console.Write("Пятница");
+else if(a == 6) Console.Write("Суббота");
+else if(a == 7) Console.Write("Воскресенье");
 else Console.Write("Число не в диапозоне 1-7");
-
-
