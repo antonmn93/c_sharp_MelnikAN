4a853e8ea65346388eb8f4f2dc17270e8b1811ca
Anton <17.11.1993@bk.ru>
2022-10-23 13:22:41 +0300

    proverka-yspex+add_proverka_01_for_proverki_tasks

--- proverka/Program.cs
+++ proverka/Program.cs
@@ -124,6 +124,11 @@
 // умноженное на само себя).
 // Например: 4->16, -3->9 -7->49
 
+// Console.WriteLine("Введите целое число");
+// int nun = Convert.ToInt32(Console.ReadLine());
+// int square = nun * nun;
+// Console.Write($"Квадрат числа {nun} = {square}"); 
+
 // Задача 1.
 // Напишите программу
 // 1. которая на вход принимает два числа
@@ -131,11 +136,31 @@
 // // a = 25, b = 5 - > да, // a = 2; b = 10 - > нет
 // // a = 9; b = - 3 - > да, // a = -3; b = 9 - > нет
 
+// Console.WriteLine("Ведите целое число");
+// int a = Convert.ToInt32(Console.ReadLine());
+
+// Console.WriteLine("Введите еще одно число");
+// int b = Convert.ToInt32(Console.ReadLine());
+
+// if (b*b == a) Console.WriteLine($" Число {a} является квадратом числа {b}");
+// else Console.WriteLine($" Число {a} не является квадратом числа {b}");
+
 // Задача 3.
 // // Напишите программу, которая будет выдавать
 // // название дня недели по заданному номеру.
 // // 3 -> Среда
 // // 5 - > Пятница
+
+// Console.WriteLine("Введите целое число в диапозоне от 1 до 7");
+// int a = Convert.ToInt32(Console.ReadLine());
+// if (a == 1) Console.WriteLine("Понедельниик");
+// else if (a == 2) Console.WriteLine("Вторник");
+// else if (a == 3) Console.WriteLine("Среда");
+// else if (a == 4) Console.WriteLine("Четверг");
+// else if (a == 5) Console.WriteLine("Пятница");
+// else if (a == 6) Console.WriteLine("Суббота");
+// else if (a == 7) Console.WriteLine("Воскресеье");
+// else Console.Write("Число не в диапозоне от 1 до 7");
 
 // Задача 5. Напишите программу, которая на вход принимеает 
 // одно число (N), а на выходе показывает все целые числа 
@@ -144,5 +169,38 @@
 
 // Первый вариант
 
+// Console.WriteLine("Введите целое число");
+// int nun = Convert.ToInt32(Console.ReadLine());
+// int nun2 = - nun;
+// while(nun2 <= nun)
+// {
+//     Console.Write(nun2+ " ");
+//     nun2++;
+// }
+
+// Console.WriteLine("Введите целое число");
+// int nun = Convert.ToInt32(Console.ReadLine());
+// int nun2 = - nun;
+// while(nun2 <= nun)
+// {
+//     Console.Write(nun2+ " ");
+//     nun2++;
+// }
 
 // Второй вариант
+
+// Console.WriteLine("Введите целое число");
+// int a = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Вывод чисел в промежутке от N до N");
+// for (int i = -a; i <= a; i++)
+// {
+//     Console.Write(i+ " ");
+// }
+
+// Console.Write("Напишите целое число ");
+// int a = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Вывод чисел в промежутке от N до N");
+// for (int i = -a; i <= a; i++)
+// {
+//      Console.Write(i + " ");
+// }
